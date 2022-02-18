2b2a2a22fa9e9ad9484724789e5da9e85b50ab1e
mads739d <madsbergholdt@gmail.com>
2022-02-18 11:52:24 +0100

    Update iir_filter.py

--- BevegelseKontekst/iir_filter.py
+++ BevegelseKontekst/iir_filter.py
@@ -7,7 +7,8 @@ data = np.array([])
 b = np.array([[22], [33], [44]])
 
 data = np.append(data, float(b[2]))
-data1 = np.append([float(b[2])], [float(b[4])], [float(b[6])])  # b[2] = xacc b[4] = acc b[6] = yrot
+data1 = np.append([float(b[0])], [float(b[1])], [float(b[2])])  # b[2] = xacc b[4] = acc b[6] = yrot
+
 ax = data1[0][0]
 print(ax)
 
