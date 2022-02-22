8340f437c8f152644d10c5f6c9298ce0d10d0db3
Martinb-hansen <bjerkov@gmail.com>
2022-02-22 09:40:15 +0100

    UploadAfData
    Upload af data og koden er opdateret

--- BevegelseKontekst/TestOfLavpasfilter.py
+++ BevegelseKontekst/TestOfLavpasfilter.py
@@ -11,13 +11,6 @@ import csv
 serialPort = sr.Serial('COM3')
 serialPort2 = sr.Serial('COM5')
 
-#serialString = ""  # Used to hold data coming over UART
-
-# YnMinusX = 0.0
-# YnMinusZ = 0.0
-# alpha = 0.02
-# g = 9.82
-
 YnMinusX = 0.0
 YnMinusZ = 0.0
 alpha = 0.02
@@ -36,7 +29,7 @@ YnMinusY = 0.0
 YnMinusXZ = 0.0
 fs=104
 
-f = open('textout.csv', 'w')
+f = open('Megadataset.csv', 'w')
 writer = csv.writer(f)
 
 while True:
@@ -46,41 +39,28 @@ while True:
             
             
             # - - - - - - - - Potentiometer - - - - - - - 
+            
+            #serialPort2.flushInput() 
             data1 = serialPort2.readline()
             data1 = data1.decode('Ascii')
             
             list_ofdata = data1.split("\t")
             
+            
             inp = float(list_ofdata[0])
             
             
-            #print("POTEN", (inp-173)/3.77)
+           # print("POTEN", (inp-173)/3.77)
             
             # - - - - - - - - Potentiometer - - - - - - - 
             
-        
+            #  - - - sportssensor - - -- - 
+            
+            serialPort.flushInput() 
             data = serialPort.readline()
             data = data.decode('Ascii')
             list_ofdata = data.split(",")
         
-         
-           # ax = math.cos(float(list_ofdata[2])) * g
-           # az = math.sin(float(list_ofdata[4])) * g
-         
-            # YnX = (1-alpha) * YnMinusX + (alpha * ax)
-            # YnMinusX = YnX
-            # YnZ = (1-alpha) * YnMinusZ + (alpha * az)
-            # YnMinusZ = YnZ
-
-            # angle = math.atan2(YnX, YnZ) #Z eller X først????
-         
-            # print(angle, "ANGLE")
-            
-            # data1 = pd.read_csv('data9.csv', sep= ',', header=None)
-            # data1.columns = ["Sample", "Temperature [°C]",  "accX", "accY", "accZ", "rotX", "rotY", "rotZ"]
-            # accx = data1['accX']
-            # accz = data1['accZ']
-            # roty = data1['rotY']
             accx = float(list_ofdata[2])
             accz = float(list_ofdata[4])
             roty = float(list_ofdata[6])
@@ -95,18 +75,20 @@ while True:
             YminusY = YnRot
         
             angle = YnRot+XZangle
-            
-        #plt.plot(preangle) #Vinkel i radianer
-        #plt.plot(YnRot) #radianer 
        
-            angle = angle*(180/3.14)
-            #plt.plot(angle)
-            #plt.show()
+            angle = angle*(180/3.14) #Omregning til grader
+            
+            #  - - - sportssensor - - -- - 
+
             print("ANGLE", angle+116, "POTEN", (inp-173)/3.77)
             
             wrut = ([angle+116, (inp-173)/3.77])
             
             writer.writerow(wrut)
+            
+            #plt.pause(0.02083333) #fs på 48 hz
+            
+            #SIDEN VI IKKE FLUSHER POTENTIOMETER; STYRES SAMPLINGEN AF DEN (48 hz)
             
         
         except KeyboardInterrupt:
